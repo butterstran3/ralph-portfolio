25f860451fc4a430c97368866d24c5264f5694c4
Jimmy Tran <jimmitran3@gmail.com>
2023-01-25 20:01:13 +1100

    Completed project pending project files

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,33 +1,114 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {BsInstagram} from 'react-icons/bs';
 import {BsTwitter} from 'react-icons/bs';
 import {BsLinkedin} from 'react-icons/bs';
+import {AiOutlineMenu} from 'react-icons/ai';
+import {RxCross1} from 'react-icons/rx';
 import './header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 
-    return (
-        <header>
-            <div className='container header__container'>
-                <div className='title__container'>
-                    <a href='/' className='title'>Ralph Santos</a>
-                </div>
-                <div className='pages__container'>
-                    <div id='work'>
-                        <a href='/' onClick={document.getElementById('work').style.textDecoration='underline'}>Work</a>
+    const [activeNav, setActiveNav] = useState('work');
+    const [mobileActive, setMobileActive] = useState(false);
+    const [mobileNavActive, setMobileNavActive] = useState(false);
+
+    const toggleNav = (page) => {
+        setActiveNav(page);
+        setMobileNavActive(!mobileNavActive);
+    }
+
+    useEffect(() => {
+        const checkIsPhone = () => {
+            if (window.innerWidth <= 600) {
+                setMobileActive(true);
+            } else {
+                setMobileActive(false);
+            }
+        };
+
+        checkIsPhone();
+        window.addEventListener('resize', checkIsPhone);
+    
+        return () => {
+            window.removeEventListener('resize', checkIsPhone);
+        };
+    }, []);
+
+
+    if (mobileActive && !mobileNavActive) {
+        return (
+                <header>
+                    <div className='container header__container'>
+                        <div className='title__container'>
+                            <a href='/' className='title'>Ralph Santos</a>
+                        </div>
+                        <div className='modal-btn__container'>
+                            <button className='modal-btn' onClick={() => setMobileNavActive(!mobileNavActive)}><AiOutlineMenu/></button>
+                        </div>
                     </div>
-                    <div id='about'>
-                        <a href='/about' onClick={document.getElementById('about').style.textDecoration='underline'}>About</a>
+                </header>
+        )
+    } else if (mobileActive && mobileNavActive) {
+        return (
+            <div className='modal__container'>
+                <header>
+                    <div className='container mobile-header__container'>
+                        <div className='title__container'>
+                            <a href='/' className='title'>Ralph Santos</a>
+                        </div>
+                        <div className='modal-btn__container'>
+                            <button className='modal-btn' onClick={() => setMobileNavActive(!mobileNavActive)}><RxCross1/></button>
+                        </div>
                     </div>
-                </div>
-                <div className='socials__container'>
-                    <a href='https://www.instagram.com/jimmitran3/' target="_blank"><BsInstagram/></a>
-                    <a href='https://twitter.com/xirenaa_' target="_blank"><BsTwitter/></a>
-                    <a href='https://www.linkedin.com/in/jimmy-tran-8885a721a/' target="_blank"><BsLinkedin/></a>
+                </header>
+                <div className='modal-content__container'>
+                    <header>
+                        <div className='container header__container'>
+                            <div className='title__container'>
+                                <a href='/' className='title'>Ralph Santos</a>
+                            </div>
+                            <div className='modal-btn__container'>
+                                <button className='modal-btn' onClick={() => setMobileNavActive(!mobileNavActive)}><RxCross1/></button>
+                            </div>
+                        </div>
+                    </header>
+                    <div className='mobile-pages__container'>
+                        <div className='mobile-work'>
+                            <Link to="/" onClick={() => toggleNav('work')} className={activeNav === 'work' ? 'active' : ''}>Work</Link>
+                        </div>
+                        <div className='mobile-about'>
+                            <Link to="/about" onClick={() => toggleNav('about')} className={activeNav === 'about' ? 'active' : ''}>About</Link>
+                        </div>
+                        <div className='mobile-socials__container'>
+                            <a href='https://www.instagram.com/jimmitran3/' target="_blank"><BsInstagram/></a>
+                            <a href='https://twitter.com/xirenaa_' target="_blank"><BsTwitter/></a>
+                            <a href='https://www.linkedin.com/in/jimmy-tran-8885a721a/' target="_blank"><BsLinkedin/></a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </header>
-    )
+        )
+    } else {
+        return (
+            <header>
+                <div className='container header__container'>
+                    <div className='title__container'>
+                        <a href='/' className='title'>Ralph Santos</a>
+                    </div>
+                    <div className='pages__container'>
+                        <Link to="/" onClick={() => setActiveNav('work')} className={activeNav === 'work' ? 'active' : ''}>Work</Link>
+                        <Link to="/about" onClick={() => setActiveNav('about')} className={activeNav === 'about' ? 'active' : ''}>About</Link>
+                    </div>
+                    <div className='socials__container'>
+                        <a href='https://www.instagram.com/jimmitran3/' target="_blank"><BsInstagram/></a>
+                        <a href='https://twitter.com/xirenaa_' target="_blank"><BsTwitter/></a>
+                        <a href='https://www.linkedin.com/in/jimmy-tran-8885a721a/' target="_blank"><BsLinkedin/></a>
+                    </div>
+                </div>
+            </header>
+        )
+    }
 }
 
 export default Header;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,46 +5,48 @@ import image2 from '../../assets/image2.jpg';
 import image3 from '../../assets/image3.jpg';
 import image4 from '../../assets/image4.jpeg';
 import image5 from '../../assets/image5.jpeg';
-import Spotlight from "../spotlight/Spotlight";
+import project1 from '../../assets/BuildingDesign2017.pdf';
 
 const data = [
     {
         id: 1,
-        image: image1
+        image: image1,
+        project_file: project1
     },
     {
         id: 2,
-        image: image2
+        image: image2,
+        project_file: project1
     },
     {
         id: 3,
         image: image3,
+        project_file: project1
     },
     {
         id: 4,
-        image: image4
+        image: image4,
+        project_file: project1
     },
     {
         id: 5,
-        image: image5
+        image: image5,
+        project_file: project1
     }
 ]
 
 const Portfolio = () => {
 
-    const imageClick = (image, pdf) => {
-        <Spotlight project={image} pdf={pdf}/>;
-    }
-
     return (
         <section id='portfolio'>
             <div className='container portfolio__container'>
                 {
-                    data.map(({id, image}) => {
+                    data.map(({id, image, project_file}) => {
                         return (
                             <article key={id} className='portfolio__item'>
                                 <div className='portfolio__item-image'>
-                                    <img className="image" src={image} onClick={imageClick(image, id)}/>
+                                    <img className="image" src={image}/>
+                                    <a className="btn view__button" href={project_file} download>Download Project</a>
                                 </div>
                             </article>
                         )
